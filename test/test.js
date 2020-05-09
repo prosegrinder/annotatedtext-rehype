fe9c68e222d2656b56fd15c6edec7cd07174c99e
David L. Day <dday376@gmail.com>
2020-05-09 14:46:52 -0400

    added tests for backslashes

--- test/test.js
+++ test/test.js
@@ -25,4 +25,23 @@ describe("#build()", function () {
     expect(result).to.equal(expected);
   });
 
+  it("should return the expected annotated text with backslashes object", function () {
+    const expected = JSON.parse(fs.readFileSync("./test/backslashes.json", "utf8"));
+    const text = fs.readFileSync("./test/backslashes.html", "utf8");
+    const result = builder.build(text);
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should match the original document with backslashes exactly", function () {
+    const expected = fs.readFileSync("./test/backslashes.html", "utf8");
+    const annotatedtext = builder.build(expected);
+    const annotation = annotatedtext.annotation;
+    let result = "";
+    for (let node of annotation) {
+      const text = node.text ? node.text : node.markup;
+      result += text;
+    }
+    expect(result).to.equal(expected);
+  });
+
 });
