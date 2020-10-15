952cfaf52ef9a1009093b065653c1fd03837ba84
David L. Day <dday376@gmail.com>
2020-10-15 06:56:43 -0400

    moved tests out of src

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 "use strict";
 
 var expect = require("chai").expect;
-var builder = require("../../out/index");
+var builder = require("../out/index");
 // var builder = require("../index");
 var fs = require("fs");
 
 describe("#build()", function () {
-
   it("should return the expected annotated text object", function () {
-    const expected = JSON.parse(fs.readFileSync("./src/test/annotatedtext.json", "utf8"));
-    const text = fs.readFileSync("./src/test/test.html", "utf8");
+    const expected = JSON.parse(
+      fs.readFileSync("./tests/annotatedtext.json", "utf8"),
+    );
+    const text = fs.readFileSync("./tests/test.html", "utf8");
     const result = builder.build(text);
     expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document exactly", function () {
-    const expected = fs.readFileSync("./src/test/test.html", "utf8");
+    const expected = fs.readFileSync("./tests/test.html", "utf8");
     const annotatedtext = builder.build(expected);
     const annotation = annotatedtext.annotation;
     let result = "";
@@ -27,14 +29,16 @@ describe("#build()", function () {
   });
 
   it("should return the expected annotated text with backslashes object", function () {
-    const expected = JSON.parse(fs.readFileSync("./src/test/backslashes.json", "utf8"));
-    const text = fs.readFileSync("./src/test/backslashes.html", "utf8");
+    const expected = JSON.parse(
+      fs.readFileSync("./tests/backslashes.json", "utf8"),
+    );
+    const text = fs.readFileSync("./tests/backslashes.html", "utf8");
     const result = builder.build(text);
     expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document with backslashes exactly", function () {
-    const expected = fs.readFileSync("./src/test/backslashes.html", "utf8");
+    const expected = fs.readFileSync("./tests/backslashes.html", "utf8");
     const annotatedtext = builder.build(expected);
     const annotation = annotatedtext.annotation;
     let result = "";
@@ -44,5 +48,4 @@ describe("#build()", function () {
     }
     expect(result).to.equal(expected);
   });
-
 });
